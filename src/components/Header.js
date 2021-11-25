c9129aaf8d3365f3fd9a79b831a4fc983d0b85a6
Akshay Sharama <76440986+codeakki@users.noreply.github.com>
2021-11-25 21:26:42 +0530

    Revert "akki"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,70 @@
-
+import { useEffect } from "react";
 import styled from "styled-components";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { auth, provider } from "../firebase";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+  setSignOutState,
+} from "../features/user/userSlice";
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push("/home");
+      }
+    });
+  }, [userName]);
+
+  const handleAuth = () => {
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          history.push("/");
+        })
+        .catch((err) => alert(err.message));
+    }
+  };
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
 
   return (
     <Nav>
       <Logo>
         <img src="/images/logo.png" class="mainLogo" alt="DigiHealth" />
       </Logo>
+
+      {!userName ? (
+        <Login onClick={handleAuth}>Login</Login>
+      ) : (
         <>
           <NavMenu>
             <a href="/home">
@@ -29,8 +85,13 @@ const Header = (props) => {
             </a>
           </NavMenu>
           <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
           </SignOut>
         </>
+      )}
     </Nav>
   );
 };
---
+++ src/components/containers/Appointment/index.js
@@ -0,0 +1,114 @@
+import React, { useMemo, useCallback } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+
+import {
+  useParams,
+  useHistory,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+
+import './styles.scss';
+
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+} from 'react-bootstrap';
+
+
+const TIME_SLOT = [9, 10, 11, 12, 13, 14, 15, 16];
+
+const Appointment = ({
+  appointments,
+}) => { 
+  let { date, month, year } = useParams();
+  const history = useHistory();
+  const currentDate = `${date}/${month}/${year}`;
+
+  const bookingData = useMemo(
+    () => ((appointments && appointments[currentDate]) || {}),
+    [appointments, currentDate]
+  );
+
+  const gotoDate = useCallback(
+    (day) => (
+      moment(`${date}/${month}/${year}`, "D/M/YYYY").add(day, "days").format('D-M-YYYY').split('-')
+    ), [date, month, year]
+  );
+
+  const changeDate = useCallback(
+    (newDate) => ( 
+      history.push(
+        `/${parseInt(newDate[0])}/${parseInt(newDate[1])}/${parseInt(newDate[2])}/`
+      )
+    ), [history]
+  )
+
+  const formatTime = useCallback(
+    (time) => {
+      if (time < 12) {
+        return `${time} AM`
+      }
+
+      if (time > 12) {
+        time = time - 12;
+      }
+    
+      return `${time} PM`
+    }, []
+  );
+
+  if (!(date && month && year)) {
+    const currentDate = moment();
+    date = currentDate.date();
+    month = currentDate.month() + 1;
+    year = currentDate.year();
+    return (
+      <Redirect to={`/${date}/${month}/${year}/`} />
+    )
+  }
+ 
+  return (
+    <div className="AppointmentWrapper">
+      <div className="navWrapper">
+        <Container className="nav">
+          <div onClick={() => changeDate(gotoDate(-1))} className="button"><img alt="left button" src="/images/left.png"/>️</div>
+          <h2>{currentDate}</h2>
+          <div onClick={() => changeDate(gotoDate(1))} className="button"><img alt="right button" src="/images/right.png"/>️</div>
+        </Container>
+      </div>
+    <Container>
+      <Row>
+        <Col className="mx-3">
+          <h2 className="mt-5">
+            Select A Time Slot
+          </h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          { TIME_SLOT.map(
+              time => (
+                <Link key={time} to={`${time}/details/`}>
+                  <Button className={bookingData[time] && "booked"}>
+                    { formatTime(time) } to { formatTime(++time) }
+                  </Button>
+                </Link>
+              )
+            )
+          }
+        </Col>
+      </Row>
+    </Container>
+    </div>
+  );
+ }
+
+export default connect(
+  ({appointments}) => ({
+    appointments,
+  })
+)(Appointment);
