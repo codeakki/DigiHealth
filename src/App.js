ae4311993960bcb0e9c9fbfabd31c3d0f1f62bf0
codeakki <codewithakki@gmail.com>
2021-11-25 11:44:35 +0530

    akki

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SimpleForm from "./components/chatbot/SimpleForm";
 import Header from "./components/Header";
-import { Provider } from 'react-redux';
 import Home from "./components/Home";
 import CovidApp from "./CovidApp";
-import Appointment from './components/containers/Appointment';
-import AppointmentDetails from './components/containers/AppointmentDetails';
 import Symptom from './Symptoms.js';
 
 function App() {
@@ -25,13 +22,6 @@ function App() {
           <Route exact path="/home/covid">
             <CovidApp />
           </Route>
-          <Route exact path="/:date/:month/:year/:timeFrom/details">
-            <AppointmentDetails />
-          </Route>
-          <Route exact path="/:date?/:month?/:year?/">
-            <Appointment />
-          </Route>
-          
         </Switch>
       </Router>
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./app/store";
-import { Provider } from "react-redux";
-import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <App />  
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-serviceWorker.unregister();
 
